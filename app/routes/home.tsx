cfe475679750e1210732bdca181c481a4e7fc59d
BrianZhao10 <brianzhaoptimal@gmail.com>
2026-02-17 20:00:41 -0500

    No more blank and bland starting screen?

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "RUbit's" },
+    { name: "description", content: "Get Out Of Scrambles Even faster and more fun!" },
   ];
 }
 
